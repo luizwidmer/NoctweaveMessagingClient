f973be592e052f429d021eb608f9f729408884b4
Widmer <115965339+crazywidmer@users.noreply.github.com>
2025-12-27 20:36:19 -0300

    Initial Commit

---
+++ PICCP Messaging Client/PICCP_Messaging_ClientApp.swift
@@ -0,0 +1,17 @@
+//
+//  PICCP_Messaging_ClientApp.swift
+//  PICCP Messaging Client
+//
+//  Created by Luiz Fernando Widmer Neto on 27/12/25.
+//
+
+import SwiftUI
+
+@main
+struct PICCP_Messaging_ClientApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
